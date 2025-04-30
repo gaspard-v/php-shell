39192e190655630d65f244fc453bc0d9c6a4279c
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2025-04-30 12:47:14 +0000

    add Config file

---
+++ src/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace GaspardV\PhpShell;
+
+class Config
+{
+    public static string $path = "/templates";
+}
+// $path = __DIR__ . 
+// static $loader = new \Twig\Loader\FilesystemLoader($path);
--- src/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace GaspardV\PhpShell;
-
-static $path = __DIR__ . "/templates";
-static $loader = new \Twig\Loader\FilesystemLoader($path);
