2c7658170a1ef4d587ee9d61065d4dea656316f0
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2025-04-30 09:59:17 +0000

    Add initial PHP source code

---
+++ src/config.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace GaspardV\PhpShell;
+
+static $path = __DIR__ . "/templates";
+static $loader = new \Twig\Loader\FilesystemLoader($path);
---
+++ src/index.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace GaspardV\PhpShell;
+
+require_once dirname(__DIR__) . "/vendor/autoload.php";
